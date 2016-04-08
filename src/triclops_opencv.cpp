bb643043dca6d062e7343b155460d504473fffc8
ros07 <ros07@example.com>
2016-04-08 18:22:42 -0400

    code cleanup

--- src/triclops_opencv.cpp
+++ src/triclops_opencv.cpp
@@ -15,83 +15,83 @@ namespace FC2T = Fc2Triclops;
 /* ConvertTriclops2Opencv converts images from multiple Point Grey API image formats to an OpenCv Mat. This function is overloaded to handle both fly capture2 and triclops image formats.
 */
 // convert a triclops color image to opencv mat
-int convertTriclops2Opencv(FC2::Image & bgrImage,
+int convertTriclops2Opencv(FC2::Image & inImage,
                            cv::Mat & cvImage){
   // convert bgr image to OpenCV Mat
-  unsigned int rowBytes = (double)bgrImage.GetReceivedDataSize()/(double)bgrImage.GetRows();
-  cvImage = cv::Mat(bgrImage.GetRows(), bgrImage.GetCols(), CV_8UC3, bgrImage.GetData(),rowBytes);
+  unsigned int rowBytes = (double)inImage.GetReceivedDataSize()/(double)inImage.GetRows();
+  cvImage = cv::Mat(inImage.GetRows(), inImage.GetCols(), CV_8UC3, inImage.GetData(),rowBytes);
   char numstr[50];
   sprintf(numstr, "rows: %d cols: %d rowbytes: %d", cvImage.rows,cvImage.cols, rowBytes);
   //putText(cvImage, numstr, cv::Point(10,cvImage.rows-30), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(100,100,250), 1, false);
 }
 
 // convert a triclops color image to opencv mat
-int convertTriclops2Opencv(TriclopsInput & bgrImage,
+int convertTriclops2Opencv(TriclopsInput & inImage,
                            cv::Mat & cvImage){
    //convert bgr image to OpenCV Mat
-      cv::Mat R(bgrImage.nrows,bgrImage.ncols,CV_8UC1,bgrImage.u.rgb.red,bgrImage.rowinc);
-      cv::Mat B(bgrImage.nrows,bgrImage.ncols,CV_8UC1,bgrImage.u.rgb.blue,bgrImage.rowinc);
-      cv::Mat G(bgrImage.nrows,bgrImage.ncols,CV_8UC1,bgrImage.u.rgb.green,bgrImage.rowinc);
+      cv::Mat R(inImage.nrows,inImage.ncols,CV_8UC1,inImage.u.rgb.red,inImage.rowinc);
+      cv::Mat B(inImage.nrows,inImage.ncols,CV_8UC1,inImage.u.rgb.blue,inImage.rowinc);
+      cv::Mat G(inImage.nrows,inImage.ncols,CV_8UC1,inImage.u.rgb.green,inImage.rowinc);
       std::vector<cv::Mat> array_to_merge;
       array_to_merge.push_back(B);
       array_to_merge.push_back(G);
       array_to_merge.push_back(R);
       cv::merge(array_to_merge,cvImage);
       char numstr[50];
-      sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, bgrImage.rowinc);
+      sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, inImage.rowinc);
       //putText(cvImage, numstr, cv::Point(10,cvImage.rows-30), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(100,100,250), 1, false);
 }
 
 // convert a triclops color image to opencv mat
-int convertTriclops2Opencv(TriclopsImage & bgrImage,
+int convertTriclops2Opencv(TriclopsImage & inImage,
                            cv::Mat & cvImage){
-  cvImage = cv::Mat(bgrImage.nrows, bgrImage.ncols, CV_8UC3, bgrImage.data,bgrImage.rowinc);
+  cvImage = cv::Mat(inImage.nrows, inImage.ncols, CV_8UC3, inImage.data,inImage.rowinc);
   char numstr[50];
-  sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, bgrImage.rowinc);
+  sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, inImage.rowinc);
   //putText(cvImage, numstr, cv::Point(10,cvImage.rows-30), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(100,100,250), 1, false);
 }
 
-// convert a triclops color image to opencv mat
-int convertTriclops2Opencv(TriclopsImage16 & bgrImage,
+// convert a triclopsImage16 disparity image to opencv mat
+int convertTriclops2Opencv(TriclopsImage16 & inImage,
                            cv::Mat & cvImage){
-  cvImage = cv::Mat(bgrImage.nrows, bgrImage.ncols, CV_16UC1, bgrImage.data,bgrImage.rowinc);
+  cvImage = cv::Mat(inImage.nrows, inImage.ncols, CV_16UC1, inImage.data,inImage.rowinc);
   char numstr[50];
-  sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, bgrImage.rowinc);
+  sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, inImage.rowinc);
   //putText(cvImage, numstr, cv::Point(10,cvImage.rows-30), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(100,100,250), 1, false);
 }
 
 
-// convert a triclops color image to opencv mat
-int convertTriclops2Opencv(TriclopsColorImage & bgrImage,
+// convert a triclopscolorImage to opencv mat
+int convertTriclops2Opencv(TriclopsColorImage & inImage,
                            cv::Mat & cvImage){
   //convert bgr image to OpenCV Mat
-     cv::Mat R(bgrImage.nrows,bgrImage.ncols,CV_8UC1,bgrImage.red,bgrImage.rowinc);
-     cv::Mat B(bgrImage.nrows,bgrImage.ncols,CV_8UC1,bgrImage.blue,bgrImage.rowinc);
-     cv::Mat G(bgrImage.nrows,bgrImage.ncols,CV_8UC1,bgrImage.green,bgrImage.rowinc);
+     cv::Mat R(inImage.nrows,inImage.ncols,CV_8UC1,inImage.red,inImage.rowinc);
+     cv::Mat B(inImage.nrows,inImage.ncols,CV_8UC1,inImage.blue,inImage.rowinc);
+     cv::Mat G(inImage.nrows,inImage.ncols,CV_8UC1,inImage.green,inImage.rowinc);
      std::vector<cv::Mat> array_to_merge;
      array_to_merge.push_back(B);
      array_to_merge.push_back(G);
      array_to_merge.push_back(R);
      cv::merge(array_to_merge,cvImage);
-//     ROS_INFO("c %d r %d rInc: %d",cvImage.cols,cvImage.rows,bgrImage.rowinc);
+//     ROS_INFO("c %d r %d rInc: %d",cvImage.cols,cvImage.rows,inImage.rowinc);
      char numstr[50];
-     sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, bgrImage.rowinc);
+     sprintf(numstr, "rows: %d cols: %d RowInc: %d", cvImage.rows,cvImage.cols, inImage.rowinc);
      //putText(cvImage, numstr, cv::Point(10,cvImage.rows-30), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(100,100,250), 1, false);
 }
 
 // convert an Opencv into a fly capture2 image
 int convertOpencv2Triclops( cv::Mat & cvImage,
-                             FC2::Image & bgrImage)
+                             FC2::Image & outImage)
 {\
   unsigned int imgStride = cvImage.step;
   int sizeImg = cvImage.rows*cvImage.cols*int(cvImage.elemSize());
-  bgrImage.SetDimensions(cvImage.rows,cvImage.cols, imgStride,FC2::PIXEL_FORMAT_BGR, FC2::NONE);
-  bgrImage.SetData(cvImage.data,sizeImg);
+  outImage.SetDimensions(cvImage.rows,cvImage.cols, imgStride,FC2::PIXEL_FORMAT_BGR, FC2::NONE);
+  outImage.SetData(cvImage.data,sizeImg);
 }
 
 // TODO This converter is broken DO NOT USE
 int convertOpencv2Triclops( cv::Mat & cvImage,
-                             TriclopsColorImage & bgrImage)
+                             TriclopsColorImage & outImage)
 {\
   int from_to[] = {0,0};
   cv::Mat b(cvImage.rows,cvImage.cols,CV_8UC1);
@@ -106,12 +106,12 @@ int convertOpencv2Triclops( cv::Mat & cvImage,
 //  int bytes = r.elemSize();
 //  unsigned int imgStride = cvImage.step;
   unsigned int imgStride = cvImage.cols;
-  bgrImage.ncols=cvImage.cols;
-  bgrImage.nrows=cvImage.rows;
-  bgrImage.rowinc=imgStride;
-  bgrImage.blue = b.data;
-  bgrImage.green = g.data;
-  bgrImage.red = r.data;// r is 76800 bytes long. sizeof r is 96 size of r.data is 8
+  outImage.ncols=cvImage.cols;
+  outImage.nrows=cvImage.rows;
+  outImage.rowinc=imgStride;
+  outImage.blue = b.data;
+  outImage.green = g.data;
+  outImage.red = r.data;// r is 76800 bytes long. sizeof r is 96 size of r.data is 8
   //ROS_INFO("c %d r %d stp: %d %zu sffride: %d",cvImage.cols,cvImage.rows,r.size().width,sizeof(b.data),imgStride);
 }
 
